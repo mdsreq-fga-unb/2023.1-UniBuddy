f035b67b0e756c517779c99ef39bd8c1948c8b04
LeonardoPadre <leonardopadre@hotmail.com>
2023-06-13 22:19:42 -0300

    feat: finaliza rota de registro

--- backend/controller/UserController.js
+++ backend/controller/UserController.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
 
 router.post("/registro", async (req, res) => {
     try {
+        const salt = await bcrypt.genSalt(10);
+
         const novo_usuario = {
             nomeCompleto: req.body.nomeCompleto,
             email: req.body.email,
-            senha: req.body.senha
+            senha: await bcrypt.hash(req.body.senha, salt)
         };
-        await User.create(novo_usuario);
-        res.status(201).json(novo_usuario);
+
+        const email = await User.findOne({where: {email: novo_usuario.email}});
+
+        if(email === null){
+            await User.create(novo_usuario);
+            res.status(201).json(novo_usuario);
+        } else {
+            res.status(400).json({ error: 'Usuário já existe' });
+        }
     } catch (error) {
         res.status(400).json({ error: 'Erro ao registrar o usuário.' });
     }
