635a39cd960e75c92ee8aa8b2e4ed3a3b20f425b
LeonardoPadre <leonardopadre@hotmail.com>
2023-06-14 00:11:30 -0300

    feat: cria tabela de caronas

---
+++ backend/models/Carona.js
@@ -0,0 +1,40 @@
+const Sequelize = require("sequelize");
+const db = require("../database/db");
+
+const Carona = db.define('caronas', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    id_usuario: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true
+    },
+    vagas: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    origem: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    destino: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    data: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    horario: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+Carona.sync();
+
+module.exports = Carona;
