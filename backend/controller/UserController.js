9d75cd846469aa0af1cad1715ac83e0f967213b4
Leonardo Fernandes Padre <62120616+LeonardoPadre@users.noreply.github.com>
2023-06-13 23:01:06 -0300

    Adiciona rotas relacionadas aos usuarios (#36)
    * feat: finaliza rota de registro
    
    * feat: finaliza rota de login

--- backend/controller/UserController.js
+++ backend/controller/UserController.js
@@ -1,19 +1,50 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
 
 router.post("/registro", async (req, res) => {
     try {
+        const salt = await bcrypt.genSalt(10);
+
         const novo_usuario = {
             nomeCompleto: req.body.nomeCompleto,
             email: req.body.email,
-            senha: req.body.senha
+            senha: await bcrypt.hash(req.body.senha, salt)
         };
-        await User.create(novo_usuario);
-        res.status(201).json(novo_usuario);
+
+        const email = await User.findOne({where: {email: novo_usuario.email}});
+
+        if(email === null){
+            await User.create(novo_usuario);
+            res.status(201).json(novo_usuario);
+        } else {
+            res.status(400).json({ error: 'Usuário já existe' });
+        }
     } catch (error) {
         res.status(400).json({ error: 'Erro ao registrar o usuário.' });
+    }
+})
+
+router.post("/login", async (req, res) => {
+    const usuario = await User.findOne({
+        attributes: ["id", "nomeCompleto", "email", "senha"],
+        where: {
+            email: req.body.email
+        }
+    });
+
+    if(usuario === null){
+        res.status(400).json({ error: 'Usuário não encontrado' });
+    } else {
+        if(!(await bcrypt.compare(req.body.senha, usuario.senha))){
+            res.status(400).json({ error: 'Senha incorreta' });
+        } else {
+            const token = jwt.sign({id: usuario.id}, "shhh");
+            res.status(400).json({ error: 'Login efetuado: ' + usuario.nomeCompleto});
+        }
     }
 })
 
