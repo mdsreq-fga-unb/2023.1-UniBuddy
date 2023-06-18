75fcd4692b841e218d4fd828c82b6b7c2d8f816d
LeonardoPadre <leonardopadre@hotmail.com>
2023-06-18 16:23:16 -0300

    fix: adiciona cors ao backend

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require("cors");
 
 const userController = require("./controller/UserController.js");
 const caronaController = require("./controller/CaronaController.js");
 const notifiController = require("./controller/NotificacoesController.js");
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.status(200).json({
