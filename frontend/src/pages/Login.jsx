4dfdcc436d8fb81153262bf0d9d2720bf0865de4
pedrolucas12 <pedrolucassantana@gmail.com>
2023-06-17 21:07:41 -0300

    Front_PedroLucas_fixes/17.06.2

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 const Login = () => {
+    const [inputs, setInputs] = useState({
+        email: "",
+        senha: "",
+    });
+    const [err, setError] = useState(null);
+
+    const navigate = useNavigate();
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setInputs({ ...inputs, [name]: value });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        console.log(inputs);
+        try {
+            const res = await axios.post("http://localhost:3000/usuarios/login", inputs);
+            console.log(res);
+            navigate("/");
+        } catch (err) {
+            console.log(err);
+        }
+        setError(err.res.data);
+    };
     return (
          <div className="auth">
             <div className="entrar">
@@ -9,10 +38,11 @@ const Login = () => {
                 <h1>Bem vindo a UniBuddy</h1>
             </div>
             <form>
-                <input required type="text" placeholder="Endereço de Email" />
-                <input required type="password" placeholder="Senha" />
-                <button className="authButton">Entrar</button>
-                <span>Você ainda não possui conta? <Link to="/register">Registrar</Link> </span>
+                <input required type="text" placeholder="Endereço de Email" name="email" onChange={handleInputChange}/>
+                <input required type="password" placeholder="Senha" name="senha" onChange={handleInputChange}/>
+                <button className="authButton" onClick={handleSubmit}>Entrar</button>
+                {err && <span>{err}</span>}
+                <span>Você ainda não possui conta??? <Link to="/register">Registrar</Link> </span>
             </form>
         </div>
     
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,18 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+    const [inputs, setInputs] = useState({
+        nome: "",
+        matricula: "",
+        email: "",
+        senha: "",
+    });
+    const [err, setError] = useState(null);
+
+    const navigate = useNavigate();
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setInputs({ ...inputs, [name]: value });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        console.log(inputs);
+        try {
+            const res = await axios.post("http://localhost:3000/usuarios/registro", inputs);
+            console.log(res);
+            navigate("/login");
+        } catch (err) {
+            console.log(err);
+        }
+        setError(err.res.data);
+    };
+
+
     return (
          <div className="auth">
             <h1>Crie o seu Perfil</h1>
             <form>
-                <input required type="text" placeholder="Nome Completo" />
-                <input required type="number" placeholder="Matricula" />
-                <input type="text" placeholder="Endereço"/>
-                <input required type="text" placeholder="Endereço de Email" />
-                <input required type="password" placeholder="Senha" />
-                <button className="authButton">Register</button>
-                <p>Error</p>
+                <input required type="text" placeholder="Nome Completo" name="nome" onChange={handleInputChange}/>
+                <input required type="number" placeholder="Matricula" name="matricula" onChange={handleInputChange}/>
+                <input required type="text" placeholder="Endereço de Email" name="email"onChange={handleInputChange}/>
+                <input required type="password" placeholder="Senha" name="senha"onChange={handleInputChange}/>
+                <button className="authButton" onClick={handleSubmit}>Register</button>
+                {err && <span>{err}</span>}
                 <span>Você já possui conta??? <Link to="/login">Login</Link> </span>
             </form>
         </div>
