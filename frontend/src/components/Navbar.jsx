a861bf50b875157505f9a7efac32a5d46f9f2876
pedrolucas12 <pedrolucassantana@gmail.com>
2023-06-19 13:14:07 -0300

    Fix_PL

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,7 +9,9 @@ const Navbar = () => {
        <div className="navbar">
             <div className="container">
                 <div className="logo">
-                    <img src={Carro} alt="Logo" />
+                    <Link to="/">
+                        <img src={Carro} alt="Logo" />
+                    </Link>
                 </div>
                 <div className="links">
                     <Link className="link" to="/write">Criar Caronas</Link>  
