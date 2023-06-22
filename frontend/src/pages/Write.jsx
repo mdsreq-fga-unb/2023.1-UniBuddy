97f85e37fb966cfe3d6e3c777a2b7be38168d135
pedrolucas12 <pedrolucassantana@gmail.com>
2023-06-22 11:46:50 -0300

    teste

--- frontend/src/pages/Write.jsx
+++ frontend/src/pages/Write.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import carro from '../img/carro.png';
 import './styles/Write.css';
+import  { useState } from "react";
 
 const Write = () => {
   const [vagas, setVagas] = useState("");
@@ -10,22 +11,27 @@ const Write = () => {
   const [data, setData] = useState("");
   const [horario, setHorario] = useState("");
 
+  const [inputsCarona, setInputsCarona] = useState({
+    vagas: "",
+    origem: "",
+    destino: "",
+    data: "",
+    horario: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setInputsCarona({ ...inputsCarona, [name]: value });
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
-    const caronaData = {
-      vagas,
-      origem,
-      destino,
-      data,
-      horario,
-    };
   
     try {
 
      // Adicione essa linha para exibir o token
     
-      const response = await fetch("http://localhost:3000/caronas/cadastrar", {
+        const response = await fetch("http://localhost:3000/caronas/cadastrar", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -45,40 +51,40 @@ const Write = () => {
     <div className="create">
       <img className="carro" src={"https://cdn-icons-png.flaticon.com/128/2300/2300372.png"} alt="carro" />
       <h1>Crie a sua Carona</h1>
-      <form className="form" onSubmit={handleSubmit}>
+      <form className="form">
         <input
           required
           type="number"
           placeholder="Quantidade de vagas"
-          value={vagas}
-          onChange={(e) => setVagas(e.target.value)}
+          name="vagas"
+          onChange={handleInputChange}
         />
         <input
           required
           type="text"
           placeholder="Origem da sua Carona"
-          value={origem}
-          onChange={(e) => setOrigem(e.target.value)}
+          name="origem"
+          onChange={handleInputChange}
         />
         <input
           type="text"
           placeholder="Destino da sua Carona"
-          value={destino}
-          onChange={(e) => setDestino(e.target.value)}
+          name="destino"
+          onChange={handleInputChange}
         />
         <input
           type="text"
           placeholder="Data da Carona"
-          value={data}
-          onChange={(e) => setData(e.target.value)}
+          name="data"
+          onChange={handleInputChange}
         />
         <input
           type="text"
           placeholder="Horario da Carona"
-          value={horario}
-          onChange={(e) => setHorario(e.target.value)}
+          name="horario"
+          onChange={handleInputChange}
         />
-        <button type="submit">Criar Carona</button>
+        <button onClick={handleSubmit}>Criar Carona</button>
       </form>
     </div>
   );
